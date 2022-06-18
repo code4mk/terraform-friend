4536d05b2f6652c10005f0f5951dc7243f5f7652
Mostafa Kamal <hiremostafa@gmail.com>
2022-06-18 06:21:32 +0600

    command

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # terraform-friend
 Terraform Friend is a terraform environment base boilerplate
+
+```bash
+chmod 777 ./friend.sh
+./friend.sh
+```
---
+++ cli/plan.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+dev_workspace=$(jq -r .dev.workspace_name ../tfriend.json)
+stage_workspace=$(jq -r .dev.workspace_name ../tfriend.json)
+prod_workspace=$(jq -r .dev.workspace_name ../tfriend.json)
+
+read -p "pick - (dev/stage/prod)? " answer
+
+if [[ $answer = "dev" ]]; then
+  eval "terraform workspace select ${dev_workspace}"
+  terraform init
+  terraform plan -var-file="./env/dev.tfvars"
+elif [[ $answer = "stage" ]]; then
+  eval "terraform workspace select ${stage_workspace}"
+  terraform init
+  terraform plan -var-file="./env/stage.tfvars"
+elif [[ $answer = "prod" ]]; then
+  eval "terraform workspace select ${prod_workspace}"
+  terraform init
+  terraform plan -var-file="./env/prod.tfvars"
+fi
